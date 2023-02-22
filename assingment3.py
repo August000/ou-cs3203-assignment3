8a236afda685f219a76d1405a6bc3ed60628391a
Augusto de Oliveira Batista <augustobatista@MacBook-Pro.local>
2023-02-21 18:33:59 -0600

    second

--- assingment3.py
+++ assingment3.py
@@ -4,3 +4,10 @@ def sum(numbers):
     for i in range(0,len(numbers)):
         totalSum += numbers[i]
     return totalSum
+
+def product(numbers):
+    totalProduct = 1
+    
+    for i in range(0,len(numbers)):
+        totalProduct *= numbers[i]
+    return totalProduct
